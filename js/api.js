3cad17ad543d08158a0d72986aa0621e5d98980e
anikateagrawal <anikateag7316@gmail.com>
2022-06-30 16:31:15 +0530

    commit

--- js/api.js
+++ js/api.js
@@ -2,35 +2,24 @@ const url="https://api.covid19api.com/summary";
 
 function fetchData()
 {
-    fetch(url).then((res)=>{return res.json()}).then((data)=>getData(data)).catch((error)=>console.log(error.message));
+    fetch(url).then((res)=>res.json()).then((data)=>{this.data=data;getData()}).catch((error)=>console.log(error.message));
 }
-function getData(data)
+var data;
+
+function getData()
 {
-    console.log(data.Countries);
+    var s=document.getElementById("country").value;
     for (let i of data.Countries)
     {
-        if(i.Country=="India")
+        if(i.Country==s)
         {
-            let country=document.createElement("span");
-            let date=document.createElement("span");
-            let tcases=document.createElement("span");
-            let tdeaths=document.createElement("span");
-            let ncases=document.createElement("span");
-            let ndeaths=document.createElement("span");
-            country.textContent="India";
-            date.textContent=`${Date()}`;
-            tcases.textContent=`${i.TotalConfirmed}`;
-            tdeaths.textContent=`${i.TotalDeaths}`;
-            ncases.textContent=`${i.NewConfirmed}`;
-            ndeaths.textContent=`${i.NewDeaths}`;
-            document.getElementById("name").appendChild(country);
-            document.getElementById("date").appendChild(date);
-            document.getElementById("tcases").appendChild(tcases);
-            document.getElementById("tdeaths").appendChild(tdeaths);
-            document.getElementById("ncases").appendChild(ncases);
-            document.getElementById("ndeaths").appendChild(ndeaths);        
+            document.getElementById("name").textContent=i.Country;
+            document.getElementById("date").textContent=new Date().toDateString();
+            document.getElementById("tcases").textContent=i.TotalConfirmed;
+            document.getElementById("tdeaths").textContent=i.TotalDeaths;
+            document.getElementById("ncases").textContent=i.NewConfirmed;
+            document.getElementById("ndeaths").textContent=i.NewDeaths;      
         }
     }
-   
 }
 fetchData();
