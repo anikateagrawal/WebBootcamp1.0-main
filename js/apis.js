46511ed0093990e8acaca316140d7be502a8e1c8
anikateagrawal <anikateag7316@gmail.com>
2022-07-06 15:47:46 +0530

    commit

---
+++ js/apis.js
@@ -0,0 +1,12 @@
+API 's
+ http://api.open-notify.org/iss-now.json
+ current location ki api
+ http://api.open-notify.org/iss-now.json?callback=CALLBACK
+ https://api.kanye.rest
+ ye api daily naye quotes degi
+ https://sunrise-sunset.org/api
+ https://opentdb.com/browse.php
+  quiz api
+ https://apilist.fun/
+ https://newsapi.org/
+ https://www.nutritionix.com/business/api
--- quizapp/logic.js
+++ quizapp/logic.js
@@ -1,82 +1,48 @@
-var count=0;
-var ans=[];
-function addquestion()
-{
-    var a=document.getElementById("questions");
-    var q=document.createElement("p");
-    q.textContent="Q."+(count+1)+" "+ document.getElementById("question").value;
-    document.getElementById("question").value=null;
-    a.appendChild(q);
-    var l=document.createElement("label");
-    l.textContent="Ans."+(count+1)+" ";
-    var option1=document.createElement("input");
-    option1.value="1";
-    option1.type="radio";
-    option1.name="answer"+(count+1);
-    a.appendChild(option1);
-    option1=document.createElement("span");
-    option1.textContent=document.getElementById("option1").value;
-    document.getElementById("option1").value=null;
-    a.appendChild(option1);
-    var option2=document.createElement("input");
-    option2.value="2";
-    option2.type="radio";
-    option2.name="answer"+(count+1);
-    a.appendChild(option2);
-    option2=document.createElement("span");
-    option2.textContent=document.getElementById("option2").value;
-    document.getElementById("option2").value=null;
-    a.appendChild(option2);
-    var option3=document.createElement("input");
-    option3.value="3";
-    option3.type="radio";
-    option3.name="answer"+(count+1);
-    a.appendChild(option3);
-    option3=document.createElement("span");
-    option3.textContent=document.getElementById("option3").value;
-    document.getElementById("option3").value=null;
-    a.appendChild(option3);
-    var option4=document.createElement("input");
-    option4.value="4";
-    option4.type="radio";
-    option4.name="answer"+(count+1);
-    a.appendChild(option4);
-    option4=document.createElement("span");
-    option4.textContent=document.getElementById("option4").value;
-    document.getElementById("option4").value=null;
-    a.appendChild(option4);
-    var option0=document.createElement("input");
-    option0.value="0";
-    option0.type="radio";
-    option0.name="answer"+(count+1);
-    option0.checked=true;
-    option0.style.display="none";
-    a.appendChild(option0);
-    ans[count++]=document.querySelector('input[name="answer"]:checked').value;
-    
+var count = 0;
+var ans = [];
+function addquestion() {
+    var sec = document.getElementById("questions");
+    sec.innerHTML = `<h1 align="center">Quiz</h1>
+    <p>Q.${count + 1} ${document.getElementById("question").value}</p>
+    <div class="obox">
+    <div><label>Ans. ${count + 1} <input type="radio" name="answer${count + 1}" value="1">
+    <span>${document.getElementById("option1").value}</span>
+    </div>
+    <div><input type="radio" name="answer${count + 1}" value="2">
+    <span>${document.getElementById("option2").value}</span>
+    </div>
+    <div><input type="radio" name="answer${count + 1}" value="3">
+    <span>${document.getElementById("option3").value}</span></div>
+    <div><input type="radio" name="answer${count + 1}" value="4">
+    <span>${document.getElementById("option4").value}</span></div> 
+    <input type="radio" name="answer${count + 1}" value="0" checked style="display:none"></div>`
+    document.getElementById("question").value = null;
+    document.getElementById("option1").value = null;
+    document.getElementById("option2").value = null;
+    document.getElementById("option3").value = null;
+    document.getElementById("option4").value = null;
+    ans[count++] = document.querySelector('input[name="answer"]:checked').value;
+
 }
-function done()
-{
-    document.getElementById("create").style.display="none";
-    var b=document.getElementById("questions");
-    var button=document.createElement("button");
-    button.textContent="submit";
-    button.onclick=check;
-    //button.addEventListener("click",check);
+function done() {
+    document.getElementById("create").style.display = "none";
+    var b = document.getElementById("questions");
+    var button = document.createElement("button");
+    button.textContent = "submit";
+    button.onclick = check;
     b.appendChild(button);
 }
-function check()
-{
-    document.getElementById("questions").style.display="none";
-    var score=0;
-    for(let i =0;i<count;i++){
-        let n="answer"+(i+1);
-        let a=document.querySelector(`input[name=${n}]:checked`).value;
-        if(a==ans[i])score++;
+function check() {
+    document.getElementById("questions").style.display = "none";
+    var score = 0;
+    for (let i = 0; i < count; i++) {
+        let n = "answer" + (i + 1);
+        let a = document.querySelector(`input[name=${n}]:checked`).value;
+        if (a == ans[i]) score++;
     }
-    var result=document.createElement("section");
+    var result = document.createElement("section");
     document.querySelector("body").appendChild(result);
-    var p=document.createElement("h1");
-    p.textContent=`Your score is ${score} out of ${count}`;
+    var p = document.createElement("h1");
+    p.textContent = `Your score is ${score} out of ${count}`;
     result.appendChild(p);
 }
